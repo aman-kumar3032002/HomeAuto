01e90cd5070c2b2f172bec58e2dd10d64b4eda88
aman-kumar3032002 <kumaraman3032002@gmail.com>
2024-04-14 21:25:31 +0530

    detecting face using harcascade method

--- opencv.py
+++ opencv.py
@@ -1,9 +1,14 @@
 import cv2
-
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture('http://192.168.1.14:81/stream')
 
 while True:
     ret, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_frame,scaleFactor = 1.1, minNeighbors= 5, minSize = (30,30))
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
     cv2.imshow("Capturing", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
