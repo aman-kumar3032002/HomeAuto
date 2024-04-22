ed5cd18612737c50e512dc6748c1466e3529a698
aman-kumar3032002 <kumaraman3032002@gmail.com>
2024-04-23 01:12:06 +0530

    updated human detection algo

--- opencv.py
+++ opencv.py
@@ -1,17 +1,50 @@
 import cv2
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture('http://192.168.1.14:81/stream')
+from imutils import imutils 
+import numpy as np
 
-while True:
-    ret, frame = cap.read()
-    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_frame,scaleFactor = 1.1, minNeighbors= 5, minSize = (30,30))
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+protopath = "/home/Pi/Desktop/Projects/MobileNetSSD_deploy.prototxt"
+modelpath = "/home/Pi/Desktop/Projects/MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
 
-    cv2.imshow("Capturing", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=600)
+
+        (H, W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+        detector.setInput(blob)
+        person_detections = detector.forward()
+
+        for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.4:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                    continue
+
+                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = person_box.astype("int")
+
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+
+        cv2.imshow("video_frame", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+
+main()
