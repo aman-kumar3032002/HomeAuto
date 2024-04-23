c5a9ae239c30bd11499f78c2ef01bfd76c9c852c
aman-kumar3032002 <kumaraman3032002@gmail.com>
2024-04-23 16:18:35 +0530

    removed graphics for recatngle and circle

--- human_tracking.py
+++ human_tracking.py
@@ -1,5 +1,5 @@
 import cv2
-import imutils
+from imutils import imutils
 import numpy as np
 #library for using Pi gpio pins
 from gpiozero import LED     
@@ -7,8 +7,8 @@ from gpiozero import LED
 light = LED(14)                                                                            #light : stores the gpio pin number, with which light is connected
 fan = LED(15)                                                                              #fan   : stores the gpio pin number, with which the fan is connected
 
-protopath = "E:\Data Science\OpenCV\HomeAutomation\MobileNetSSD_deploy.prototxt"
-modelpath = "E:\Data Science\OpenCV\HomeAutomation\MobileNetSSD_deploy.caffemodel"
+protopath = "/home/Pi/Desktop/Projects/MobileNetSSD_deploy.prototxt"
+modelpath = "/home/Pi/Desktop/Projects/MobileNetSSD_deploy.caffemodel"
 #using a pretrained model-------------------------------------------------------------------
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)   
 
@@ -28,11 +28,11 @@ def main():
     while True:
         ret, frame = cap.read()
         frame = imutils.resize(frame, width=600)
-        cv2.rectangle(frame, (boundry_start_x,boundry_start_y), (boundry_end_x,boundry_end_y) , (0, 0, 255), 2)
+        # cv2.rectangle(frame, (boundry_start_x,boundry_start_y), (boundry_end_x,boundry_end_y) , (0, 0, 255), 2)
 
         (H, W) = frame.shape[:2]
 
-        blob = cv2.dnn.blobFromImage(frame, 1/255, (W, H), 255)                             #blob: stores the 4d array for the input image
+        blob = cv2.dnn.blobFromImage(frame,  0.007843, (W, H), 127.5)                       #blob: stores the 4d array for the input image
 
         #setting the input layer as the costme
         detector.setInput(blob)
@@ -40,9 +40,8 @@ def main():
         # print(person_detections)
         for i in np.arange(0, person_detections.shape[2]):
             confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.2:
+            if confidence > 0.4:
                 idx = int(person_detections[0, 0, i, 1])
-                print(idx)
                 if CLASSES[idx] != "person":
                     continue
 
@@ -58,7 +57,7 @@ def main():
                 # cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
                 #drawing a point on the centre of the box
-                cv2.circle(frame,(detected_x,detected_y),3,(255,0,0),-1)
+                # cv2.circle(frame,(detected_x,detected_y),3,(255,0,0),-1)
 
         cv2.imshow("Application", frame)
         key = cv2.waitKey(1)
